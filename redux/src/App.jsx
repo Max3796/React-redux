bd0ad370fd10fd2641a2e2925f456452f991751f
Max3796 <gurpreet.singh@kalvium.community>
2024-04-26 15:22:45 +0530

    Done

---
+++ redux/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react';
+import LikeCount from './components/LikeCount';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <LikeCount />
+    </div>
+  )
+}
+
+export default App
